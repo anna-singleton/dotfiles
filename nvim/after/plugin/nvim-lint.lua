00b6552849fa24751562f07b31bde74eda4a2796
anna-singleton <annabeths111@gmail.com>
2023-04-19 15:39:49 +0100

    setup flake8 so it doesnt step on pyright's shoes

--- nvim/after/plugin/nvim-lint.lua
+++ nvim/after/plugin/nvim-lint.lua
@@ -2,6 +2,14 @@ require('lint').linters_by_ft = {
   python = {'flake8'},
 }
 
+local flake8 = require('lint').linters.flake8
+flake8.args = {
+
+    '--select',
+    'E,W',
+    "--format=%(path)s:%(row)d:%(col)d:%(code)s:%(text)s",
+    "--no-show-source", "-" }
+
 vim.api.nvim_create_autocmd({ "InsertLeave" }, {
   callback = function()
     require("lint").try_lint()
