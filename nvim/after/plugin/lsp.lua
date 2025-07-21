c26356e3205206a4a10e83f7f1ee8bd51d7e6b6e
Anna Singleton <annabeths111@gmail.com>
2025-07-21 12:14:21 +0100

    fix(nvim-roslyn): attach correctly

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -249,6 +249,69 @@ require('csharp').setup({
     },
 })
 
+local roslyn_attach_func = (function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>aa", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>ar", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>af", vim.lsp.buf.format)
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+    if client.server_capabilities.signatureHelpProvider then
+        require('lsp-overloads').setup(client, {
+            -- UI options are mostly the same as those passed to vim.lsp.util.open_floating_preview
+            ui = {
+                border = "single", -- The border to use for the signature popup window. Accepts same border values as |nvim_open_win()|.
+                height = nil,      -- Height of the signature popup window (nil allows dynamic sizing based on content of the help)
+                width = nil,       -- Width of the signature popup window (nil allows dynamic sizing based on content of the help)
+                wrap = true,       -- Wrap long lines
+                wrap_at = nil,     -- Character to wrap at for computing height when wrap enabled
+                max_width = nil,   -- Maximum signature popup width
+                max_height = nil,  -- Maximum signature popup height
+                -- Events that will close the signature popup window: use {"CursorMoved", "CursorMovedI", "InsertCharPre"} to hide the window when typing
+                close_events = { "CursorMoved", "BufHidden", "InsertLeave" },
+                focusable = true,                       -- Make the popup float focusable
+                focus = false,                          -- If focusable is also true, and this is set to true, navigating through overloads will focus into the popup window (probably not what you want)
+                offset_x = 0,                           -- Horizontal offset of the floating window relative to the cursor position
+                offset_y = 0,                           -- Vertical offset of the floating window relative to the cursor position
+                floating_window_above_cur_line = false, -- Attempt to float the popup above the cursor position
+                -- (note, if the height of the float would be greater than the space left above the cursor, it will default
+                -- to placing the float below the cursor. The max_height option allows for finer tuning of this)
+                silent = true, -- Prevents noisy notifications (make false to help debug why signature isn't working)
+                -- Highlight options is null by default, but this just shows an example of how it can be used to modify the LspSignatureActiveParameter highlight property
+                highlight = {
+                    italic = true,
+                    bold = true,
+                    fg = "#ffffff",
+                }
+            },
+            keymaps = {
+                next_signature = "<C-j>",
+                previous_signature = "<C-k>",
+                next_parameter = "<C-l>",
+                previous_parameter = "<C-h>",
+                close_signature = "<A-s>"
+            },
+            display_automatically = true -- Uses trigger characters to automatically display the signature overloads when typing a method signature
+        })
+    end
+end)
+
 require('roslyn').setup({
-    on_attach = attach_func
+    on_attach = roslyn_attach_func
+})
+
+vim.lsp.config("roslyn", {
+    on_attach = roslyn_attach_func,
+    settings = {
+        ["csharp|symbol_search"] = {
+            dotnet_search_reference_assemblies = true,
+        }
+    }
 })
